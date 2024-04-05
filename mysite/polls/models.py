1aaedbfdb978918af9363713be66fd1843e5c442
Jad Samara <jadsamara@yahoo.com>
2024-04-06 00:37:28 +0300

    tutorial part 2

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,38 @@
-from django.db import models
+"""
+Models module fot the app: Polls
 
-# Create your models here.
+has:
+- Question
+- Choice
+"""
+from datetime import timedelta
+from django.db import models
+from django.utils import timezone
+
+class Question(models.Model):
+    """
+    Question model
+    """
+    question_text = models.CharField(max_length=256)
+    pub_date = models.DateTimeField("date published")
+
+    def __str__(self):
+        return str(self.question_text)
+
+    def was_published_recently(self):
+        """
+        checks if the question was published in the last day
+        """
+        return self.pub_date >= timezone.now() - timedelta(days=1)
+
+
+class Choice(models.Model):
+    """
+    Choice model
+    """
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=256)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return str(self.choice_text)
