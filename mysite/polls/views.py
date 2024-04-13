07a3ed795f755626f4504cbd6d52f6b1df95c2a8
Jad Samara <jadsamara@yahoo.com>
2024-04-13 23:34:56 +0300

    views placeholders

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import HttpResponse, get_object_or_404, get_list_or_404
+from . models import Question, Choice
 
 def index(request):
-    """
-    Index Page
-    """
-    print(request)
-    return HttpResponse("Hello world")
+    latest_questions = Question.objects.order_by("-pub_date")[:5]
+    result = "<br>".join([q.question_text for q in latest_questions])
+    return HttpResponse(result)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return HttpResponse(f"You're looking at question {question_id}: {question.first()}")
+    
+def results(request, question_id):
+    response = "You're looking at the results of %s"
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question % s", question_id)
